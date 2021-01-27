032fd154a7b3375d9e0200557c6283672852bb61
Pedro Lacerda <pedrolacerda@github.com>
2021-01-26 21:21:01 -0300

    challenging myself stupidly

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router
+.get('/', function(req, res, next) {
   res.send('respond with a resource');
+})
+.get('/cool', (req, res, next) => {
+  res.send("You're so cool");
 });
 
 module.exports = router;
